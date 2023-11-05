1ba83ae3577847244c392cd081a192ef7bfc43c4
kang <kjxstar@gmail.com>
2023-11-05 20:20:35 +0800

    setup neotree

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,18 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup({
+        enable_git_status = false
+    })
+    vim.keymap.set("n", "<M-1>", ":Neotree toggle<cr>")
+  end,
+}
