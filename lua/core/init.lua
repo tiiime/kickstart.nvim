c45cdbc8ef0fecc9158ef618002362ada009e449
kang <kjxstar@gmail.com>
2023-11-13 21:35:58 +0800

    update local.properties impl

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,7 +1,36 @@
-local g = vim.g
+local function read_local_config_file()
+	local file = io.open(vim.fn.stdpath('config') .. "/config/local.properties", "r")
+	local map = {}
+	if not file then
+		return nil
+	end
+	-- 逐行读取文件内容
+	for line in file:lines() do
+		if line == "" or line:match("^%s*#") then
+			goto continue
+		end
+		-- 使用正则表达式匹配键值对
+		local key, value = line:match("([^=]+)=(.+)")
 
--- Mac
-g.space_lhs = "D"
+		-- 如果匹配成功则将键值对存入 map 中
+		if key and value then
+			map[key] = value
+		end
+		::continue::
+	end
 
--- Win/Linux
--- g.space_lhs = "M"
+	-- 关闭文件
+	file:close()
+	return map
+end
+
+
+local config = read_local_config_file()
+
+if config == nil then
+	return
+end
+
+for k, v in pairs(config) do
+	vim.g[k] = v
+end
--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -11,9 +11,16 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
+  config = function()
     require('neo-tree').setup({
-        enable_git_status = false
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+      enable_git_status = false,
     })
     vim.keymap.set("n", "<" .. g.space_lhs .. "-1>", ":Neotree toggle<cr>")
   end,
